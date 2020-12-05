ce2fbcd41a356715441b62884f17c787d145aba8
Hermer29 <kliman.chan@gmail.com>
2020-12-05 23:18:35 +0300

    Added subtract function, and copy for string args

--- mainfile.py
+++ mainfile.py
@@ -2,3 +2,8 @@ def add(a : int, b : int):
     return a+b
 def add_str(a : str, b : str):
     return int(a) + int(b)
+
+def sub(a : int, b : int):
+    return a-b
+def sub_str(a : str, b : str):
+    return int(a)-int(b)
